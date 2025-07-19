06033cf74fa4aa39167e7a4b020a674ba1544968
Madeline Conter <madelineconter2027@u.northwestern.edu>
2025-07-18 20:31:52 -0500

    adding Naka's new libraries

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import os # file and directory management
 import pickle
 import constants as cons
 import models as mod
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # CONFIGURATION
 print("Starting configuration!")
