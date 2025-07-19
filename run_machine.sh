9a60c26c88a4ab9a525841de9ee733ab5aa21baa
Madeline Conter <madelineconter2027@u.northwestern.edu>
2025-07-18 20:06:29 -0500

    Fixing bash script

--- run_machine.sh
+++ run_machine.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-ENV_NAME = "anomaly-env"
-PYTHON_SCRIPT = "main.py"
+ENV_NAME="anomaly-env"
+PYTHON_SCRIPT="main.py"
 
-if [[{ "$VIRTUAL_ENV" != ""}]]; then
-    echo "Virtual Env Already On"
+# Check if virtual environment is already activated
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Virtual environment already activated."
 else
-    echo "Activating Virtual Env"
-    source ~/$ENV_NAME/bin/activate
+    echo "Activating virtual environment..."
+    source ~/"$ENV_NAME"/bin/activate
 fi
 
-echo "Running Python Script"
-python3 $PYTHON_SCRIPT
-
-
+echo "Running Python script..."
+python3 "$PYTHON_SCRIPT"
 
