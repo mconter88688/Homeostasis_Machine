a6722964e6310f1d79cd0df5dadc14f7c3097090
mconter88688 <maddyconter2718@gmail.com>
2025-08-18 22:19:15 -0500

    changes to data class for more robust data managemnet

--- main.py
+++ main.py
@@ -39,10 +39,13 @@ class Data:
         if os.path.exists(feedback_file):
             try:
                 with open(feedback_file, "rb") as f:
-                    self.normal_data, self.ld_normal_data, self.rd03_normal_data = pickle.load(f)
-            except EOFError:
+                    dict_data = pickle.load(f)
+                    self.normal_data = dict_data.get("image", [])
+                    self.ld_normal_data = dict_data.get("ld", [])
+                    self.rd03_normal_data = dict_data.get("rd03", [])
+            except (EOFError, pickle.UnpicklingError, AttributeError, ValueError) as e:
                 self.normal_data, self.ld_normal_data, self.rd03_normal_data = [], [], []
-                print("File is empty")
+                print(f"Failed to load data ({type(e).__name__}): {e}")
         else:
             self.normal_data, self.ld_normal_data, self.rd03_normal_data = [], [], []
             print("file does not exist")
@@ -62,8 +65,19 @@ class Data:
         self.rd03_normal_data.append(new_data)
 
     def save_data(self, feedback_file):
-        with open(feedback_file, "wb") as f:
-            pickle.dump((self.normal_data, self.ld_normal_data, self.rd03_normal_data), f)
+        tmp_file = feedback_file + ".tmp"
+        try:
+            with open(tmp_file, "wb") as f:
+                pickle.dump({"image": self.normal_data, 
+                            "ld": self.ld_normal_data,
+                            "rd03": self.rd03_normal_data}, f)
+            os.replace(tmp_file, feedback_file)
+        finally:
+            if os.path.exists(tmp_file):
+                try:
+                    os.remove(tmp_file)
+                except OSError:
+                    pass
 
     def is_empty(self):
         return (len(self.normal_data) == 0 and len(self.ld_normal_data) == 0 and len(self.rd03_normal_data) == 0)
